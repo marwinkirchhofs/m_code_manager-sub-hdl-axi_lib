78d744b6dcfaf1e2da023a9c251d2a19ced51c40
Marwin Kirchhofs <m.c.kirchhofs@student.tudelft.nl>
2024-11-27 12:22:34 +0100

    ifc data stream (temp): remove clocking block for simulation compatibility, add a hs_ signal to use instead of hs() function (leave the function for backwards comp.)

--- ifc_data_stream_hs.sv
+++ ifc_data_stream_hs.sv
@@ -1,13 +1,15 @@
 `timescale 1ns/1ps
 
 interface ifc_data_stream_hs #(
-    parameter int DATA_WIDTH = 32
+    parameter int               DATA_WIDTH = 32,
+    parameter real              T_SETUP = 0.9,
+    parameter real              T_CTOQ = 0.3
 ) (
     input clk,
     input rst_n
 );
 
-    localparam int STRB_WIDTH = DATA_WIDTH/8;
+    localparam int STRB_WIDTH = $ceil(DATA_WIDTH/8);
 
     logic [DATA_WIDTH-1:0]          data;
     logic [STRB_WIDTH-1:0]          strb;
@@ -15,18 +17,35 @@ interface ifc_data_stream_hs #(
     logic                           valid;
 
     // shortcut to avoid having to write `(if_<...>.ready & if_<...>.valid)`
-    function hs();
+    function automatic hs();
         return ready & valid;
     endfunction
 
+    // (named hs_ to not interfere with existing usage of hs() )
+    logic hs_;
+    assign hs_ = ready & valid;
+
+    // TODO: I might need a way to put the clocking block back in, but still use 
+    // the interface in a testbench with assignments (instead of accessing via 
+    // the modport in a test agent). Currently that is causig a 'written by both 
+    // continuous and procedural assignment' error.
+//     clocking cb @(posedge clk);
+//         default input #T_SETUP output #T_CTOQ;
+//         input ready;
+//         output data, valid, strb;
+//     endclocking;
+
     modport master (
+//         clocking cb,
         input ready,
-        output data, valid, strb
+        output data, valid, strb,
+        import hs, hs_
     );
 
     modport slave (
         output ready,
-        input data, valid, strb
+        input data, valid, strb,
+        import hs, hs_
     );
 
 endinterface
